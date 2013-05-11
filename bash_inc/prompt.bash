c4563e348ab461d7c0c8cd78c6afacf62499a661
David Numan <dave@gv.ca>
2013-05-11 19:44:14 -0400

    prompt changes

--- bash_inc/prompt.bash
+++ bash_inc/prompt.bash
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 function prompt() {
-  local git_branch=" $(git_short_sha)$(scm_prompt_info) "
+  local git_branch="$(git_short_sha)$(scm_prompt_info)"
+  if [[ $git_branch ]]
+  then
+    git_prompt=" $git_branch "
+  else
+    git_prompt=" "
+  fi
+
   local prompt_char='» '
 #  [[ $(grep `pwd` ~/.drush/*aliases.drushrc.php) ]] && prompt_char='∞ '
 
-  PS1="$red$(scm_char) ${reset_color}\u@\H $yellow\w${reset_color}$git_branch$red$prompt_char$reset_color"
+  PS1="$red$(scm_char) ${reset_color}\u@\H $yellow\w${reset_color}$git_prompt$prompt_char$reset_color"
   PS2='> '
   PS4='+ '
 }
@@ -13,5 +20,8 @@ function prompt() {
 
 PROMPT_COMMAND=prompt
 
-SCM_THEME_PROMPT_DIRTY=" ✗"
-SCM_THEME_PROMPT_CLEAN=" ✓"
+SCM_THEME_PROMPT_DIRTY=" $red✗${reset_color}"
+SCM_THEME_PROMPT_CLEAN=" $green✓${reset_color}"
+SCM_THEME_PROMPT_PREFIX='|'
+SCM_THEME_PROMPT_SUFFIX=''
+
