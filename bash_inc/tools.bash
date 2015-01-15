1ce82673fb2e620ac24d75257b26ca6de829d9ff
David Numan <david.numan@civicactions.com>
2015-01-15 09:13:14 -0500

    adding gtop function

--- bash_inc/tools.bash
+++ bash_inc/tools.bash
@@ -4,6 +4,16 @@ function gwatch {
 while true; do
   git remote update
   git log --graph --date=relative --all
-  sleep 120
+  sleep 1
 done
+}
+
+gtop ()
+{
+  while true; do
+    clear;
+    git status -s;
+    git log -5;
+    sleep 4;
+  done
 }
